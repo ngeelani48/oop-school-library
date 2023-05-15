eaa29868d4c950da662f36d952044b077798d8fb
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-15 22:52:33 +0300

    create class Person

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  attr accessor :name, :age, :id
+
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1...1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
