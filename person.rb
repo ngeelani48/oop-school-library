2e6af75b37d64c4e67b993efbd720be635c1fbce
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-17 11:19:42 +0300

    turn Person calss to Nameable

--- person.rb
+++ person.rb
@@ -1,7 +1,9 @@
-class Person
+require './nameable'
+class Person < Nameable
   attr_accessor :name, :age, :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super(name)
     @id = Random.rand(1...1000)
     @age = age
     @name = name
@@ -10,6 +12,10 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
