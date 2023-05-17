d654afdb687db8f7dca7dec96d85b662d69753ab
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-17 17:22:47 +0300

    add test code to person.rb file

--- person.rb
+++ person.rb
@@ -1,9 +1,10 @@
-require './nameable'
-
+require_relative './nameable'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
 class Person < Nameable
   attr_accessor :name, :age, :id
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age:, name: 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1...1000)
     @age = age
@@ -25,3 +26,10 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+person = Person.new(age: 22, name: 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(nameable: person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(nameable: capitalized_person)
+puts capitalized_trimmed_person.correct_name
