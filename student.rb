2fa9b183cae80fcc73a573db01539ed5d29499b3
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-18 09:49:14 +0300

    create a class Classroom

--- student.rb
+++ student.rb
@@ -2,11 +2,16 @@ require './person'
 
 class Student < Person
   def initialize(classroom)
-    super()
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯(ツ)/¯'
+    '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
