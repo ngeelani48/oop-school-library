3c7ed6d76938ad749b19b5f33cc151ba711c4411
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-18 11:14:54 +0300

    update Person class

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative './nameable'
 require_relative './capitalize_decorator'
 require_relative './trimmer_decorator'
 class Person < Nameable
-  attr_accessor :name, :age, :id
+  attr_accessor :name, :age, :id, :rentals
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
     super()
@@ -10,6 +10,7 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -20,19 +21,14 @@ class Person < Nameable
     @name
   end
 
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
-
-# for seeing decorators in action
-# some names are updated for fixing the linters
-
-person = Person.new(age: 22, name: 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(nameable: person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(nameable: capitalized_person)
-puts capitalized_trimmed_person.correct_name
