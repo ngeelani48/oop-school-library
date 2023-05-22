605c5dfdfa9a711fc117e0d40ea32ce654c1dc3a
Kelvin <ngotho.kelvin@yahoo.com>
2023-05-22 17:16:49 +0300

    Refactor App.rb &  menu functionality

--- app.rb
+++ app.rb
@@ -127,5 +127,43 @@ class App
     end
     rentals_by_id
   end
+
+  def run
+    welcome
+
+    loop do
+      puts 'Please choose an option by entering a number:'
+      puts "\t1 - List all books"
+      puts "\t2 - List all people"
+      puts "\t3 - Create a person"
+      puts "\t4 - Create a book"
+      puts "\t5 - Create a rental"
+      puts "\t6 - List all rentals for a given person id"
+      puts "\t7 - Exit"
+
+      option = gets.chomp
+
+      case option
+      when '1'
+        display_books
+      when '2'
+        display_people
+      when '3'
+        add_new_person
+      when '4'
+        create_book
+      when '5'
+        rent_book
+      when '6'
+        list_rentals
+      when '7', 'q', 'quit'
+        break
+      else
+        puts 'Invalid option. Please try again.'
+      end
+    end
+
+    puts 'Thank you for using this app!'
+  end
 end
 
