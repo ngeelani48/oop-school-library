067dc9470afed58c10b87bcf64018f3633870b78
Kelvin <ngotho.kelvin@yahoo.com>
2023-05-25 17:24:12 +0300

    Modify tests for rental class

--- tests/rental_spec.rb
+++ tests/rental_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../rental'
+require_relative '../person'
 
 RSpec.describe Rental do
   let(:date) { '2023-05-25' }
   let(:book) { instance_double('Book') }
-  let(:person) { instance_double('Person') }
+  let(:person) { Person.new } # Creating an actual instance of Person
 
   subject { described_class.new(date, book, person) }
 
@@ -17,8 +18,8 @@ RSpec.describe Rental do
     end
 
     it 'adds the rental to the book' do
-      expect(book).to receive(:rentals) << subject
-      expect { subject }.to change { book.rentals.count }.by(1)
+      expect(book).to receive(:add_rental).with(subject) # Using add_rental method
+      subject # Creating the Rental object
     end
 
     it 'sets the person' do
@@ -26,8 +27,8 @@ RSpec.describe Rental do
     end
 
     it 'adds the rental to the person' do
-      expect(person).to receive(:rentals) << subject
-      expect { subject }.to change { person.rentals.count }.by(1)
+      expect(person).to receive(:add_rental).with(subject) # Using add_rental method
+      subject # Creating the Rental object
     end
   end
 end
